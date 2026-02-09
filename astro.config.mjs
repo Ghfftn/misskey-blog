eccc6172a90b4bae6250df4d8231da7caddd3cfd
Ghfftn <netchen1818@gmail.com>
2026-02-09 05:02:55 +0100

    final version: diffuse ambient light

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // 这里必须是 server，表示开启服务端渲染
+  adapter: cloudflare()
+});
